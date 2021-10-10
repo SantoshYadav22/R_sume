3fe87314c403518c97d4d779fe76f930d71227dd
Rahul <santoshbusiness108@gmail.com>
2021-10-10 19:01:55 +0530

    restFrame

--- Resume/Resume_detail/urls.py
+++ Resume/Resume_detail/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('run/', views.runs),
     path('contact/', views.cores,name='contact'),
     path('contacts/', views.kores,name='about'),
+    path('student/<int:inp>',views.student_detail),
+    path('student_/',views.student_list),
     
 ]
+ 
--- Resume/Resume_detail/views.py
+++ Resume/Resume_detail/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import serializers
+
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 # Create your views here.
 def run(request):
@@ -13,3 +19,18 @@ def cores(request):
 
 def kores(request):
     return render(request,'corehtml/contact.html')
+
+#model object -single student data
+
+def student_detail(request,inp):
+    stu=Student.objects.get(id=inp)
+    serializer=StudentSerializer(stu)
+    json_data=JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,content_type='application/json')
+
+
+def student_list(request):
+    stu1=Student.objects.all()
+    serializer1=StudentSerializer(stu1, many=True)
+    json_data=JSONRenderer().render(serializer1.data)
+    return HttpResponse(json_data,content_type='application/json')
